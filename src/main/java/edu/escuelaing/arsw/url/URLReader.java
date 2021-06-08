23c2876885f1ae15f83197b8676ab07e22d5beb7
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-06-08 12:07:39 -0500

    Datagrams definition

---
+++ src/main/java/edu/escuelaing/arsw/url/URLReader.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arsw.url;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLReader {
+
+    public static void main(String... args) throws MalformedURLException {
+        URL google = new URL("http://www.google.com/");
+        System.out.println("---- Leyendo los datos de un objeto URL ----");
+        System.out.println("Protocol: " + google.getProtocol());
+        System.out.println("Authority: " + google.getAuthority());
+        System.out.println("Host: " + google.getHost());
+        System.out.println("Port: " + google.getPort());
+        System.out.println("Path: " + google.getPath());
+        System.out.println("Query: " + google.getQuery());
+        System.out.println("File: " + google.getFile());
+        System.out.println("Reference: " + google.getRef());
+    }
+}
